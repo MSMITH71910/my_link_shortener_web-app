f872090d1786de35d1efb2e294280cb02c082c6f
MSMITH71910 <m.smith71910@gmail.com>
2025-03-29 17:11:11 -0400

    updated settings, build.sh, requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-
+WSGI_APPLICATION = 'config.wsgi.application'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
